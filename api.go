ddec8752bd7c42a4370624661fac2698262635a3
mallvielfrass <masterharoldsaxon4@gmail.com>
2018-11-13 11:46:30 +0300

    reborn

---
+++ api.go
@@ -0,0 +1,22 @@
+package pretiumvkgo
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type API struct {
+	httpClient *http.Client
+
+	Key string
+}
+
+func NewAPI(key string) *API {
+	return &API{
+		httpClient: &http.Client{},
+		Key:        key}
+}
+
+func (api *API) Hello() {
+	fmt.Println("Hello world!")
+}
