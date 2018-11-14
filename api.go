1d4d3e55bf2328e4453ef0c930d835e21b234be0
mallvielfrass <masterharoldsaxon4@gmail.com>
2018-11-14 12:08:40 +0300

    add method users_get

--- api.go
+++ api.go
@@ -3,6 +3,8 @@ package pretiumvkgo
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
+	"bytes"
 )
 
 type API struct {
@@ -19,6 +21,25 @@ func NewAPI(key string) *API {
 
 func (api *API) Hello() {
 	fmt.Println("Hello world!")
-	fmt.Println(key)
-	fmt.Println("key")
+	//httpClient.Get(url)
+}
+func (api *API) users_get(ids string, fields string) string{
+	fmt.Println("Hello world!")
+	//httpClient.Get(url)
+
+	url := "https://api.vk.com/method/users.get?"+"&access_token="+api.Key+"&v=5.87"
+	rule := `{"user_ids":`+ids+`,"fields"=`+fields+`}`
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(rule)))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("http request error:", err)
+	}
+	defer resp.Body.Close()
+	bodyBuf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("response body read error:", err)
+	}
+	result:=string(bodyBuf)
+	return result
 }
