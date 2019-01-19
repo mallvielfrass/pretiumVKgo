d95390c8f67a51205b34a15d7359ed31ccb7dbad
mallvielfrass <masterharoldsaxon4@gmail.com>
2019-01-19 04:30:30 +0300

    Add groups.search

--- api.go
+++ api.go
@@ -1,11 +1,11 @@
 package pretiumvkgo
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 type API struct {
@@ -24,8 +24,8 @@ func (api *API) Hello() {
 	fmt.Println("Hello world!")
 	//httpClient.Get(url)
 }
-func conventus (cmeth string,crule string) string{
-	url := "https://api.vk.com/method/"cmeth+"?"+"&access_token="+api.Key+"&v=5.87"
+func (api *API) conventus(cmeth string, crule string) string {
+	url := "https://api.vk.com/method/" + cmeth + "?" + "&access_token=" + api.Key + "&v=5.58"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(crule)))
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := api.httpClient.Do(req)
@@ -37,14 +37,38 @@ func conventus (cmeth string,crule string) string{
 	if err != nil {
 		log.Fatal("response body read error:", err)
 	}
-	result:=string(bodyBuf)
+	result := string(bodyBuf)
 	return result
 }
-func (api *API) Users_get(ids string, fields string) string{
-	fmt.Println("Hello world!")
+func (api *API) Users_get(ids string, fields string) string {
+	fmt.Println("Users_get")
 	//httpClient.Get(url)
-	method:="users.get"
-	rule := `{"user_ids":`+ids+`,"fields"=`+fields+`}`
-	retres:= conventus(method,rule)
+	method := "users.get"
+	rule := `{"user_ids":` + ids + `,"fields"=` + fields + `}`
+	retres := api.conventus(method, rule)
+	return retres
+}
+
+func (api *API) Groups_search(q string, offset string, count string) string {
+	fmt.Println("groups.search")
+	//httpClient.Get(url)
+	method := "groups.search"
+	rule := `{"q":` + q + `,"offset"=` + offset + `,"count"=` + count + `}`
+	retres := api.conventus(method, rule)
+	return retres
+}
+func (api *API) Groups_searchx(q string) string {
+	fmt.Println("groups.search")
+	//httpClient.Get(url)
+	method := "groups.search"
+	rule := `{"q":` + q + `}`
+	retres := api.conventus(method, rule)
+	return retres
+}
+func (api *API) GetProfileInfo() string {
+
+	method := "account.getProfileInfo"
+	rule := ``
+	retres := api.conventus(method, rule)
 	return retres
 }
